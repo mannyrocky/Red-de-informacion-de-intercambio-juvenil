4ec839b9972cf45bc05ae4c0d07c1f4eaaf725c5
mannyrocky <manuel.villanueva96@hotmail.com>
2019-05-12 01:26:03 -0600

    se agregaron los modales con los que se va a trabajar falta agregar los modales que van a servir para agregar a la basede datos falta hacer el metodo que me cambie la info de la pagina y hacer el menu dinamico

---
+++ public/assets/js/Eventos4.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $(".noticias").click(function(){
+        var form = "";
+        var titulo = $("#modalTitle4");
+        var body = $("#bodyModal4");
+        form+='<form><div class="row"><div class="col">';
+       form+='<input type="text" class="form-control" placeholder="Id"></div><div class="col"><input type="text" class="form-control" placeholder="Titulo"></div></div>';
+       form+='<div class="row mt-4"><div class="col"><input type="text" class="form-control" placeholder="Autor"></div><div class="col"><input type="text" class="form-control" placeholder="Fecha"></div></div></form>';
+       form+='<div class="row mt-4"><div class="col"><textarea rows="4" class="form-control" placeholder="Descrpicion"></textarea></div></div>';
+       form+='<div class="row mt-4 justify-content-center"><input class="btn btn-info" name="uploadedfile" type="file" /></div>';
+       body.html(form);
+       titulo.html('Editar Noticias');
+    });
+});
