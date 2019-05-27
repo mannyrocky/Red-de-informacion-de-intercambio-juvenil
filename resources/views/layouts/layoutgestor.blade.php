ef55b7aee502c5503a4f096a190e8d3a2309540a
mannyrocky <manuel.villanueva96@hotmail.com>
2019-05-26 19:38:36 -0600

    modales agregados faltan el eliminar en conjunto el de imagen y las validaciones nada mas y cargar las otras cosas dinamicamente

--- resources/views/layouts/layoutgestor.blade.php
+++ resources/views/layouts/layoutgestor.blade.php
@@ -389,6 +389,92 @@
     </div>
   </div>
 </div>
+<div class="modal fade" id="editarevento" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="modalTitle2">Editar Evento</h5>
+        <button type="button" class="close" data-dismiss="modal2" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body" id="bodyModal">
+        <form>
+          <div class="row">
+            <div class="col">
+              <input type="text" class="form-control" placeholder="Id"  id="ids" disabled>
+            </div>
+            <div class="col">
+              <input type="text" class="form-control" placeholder="Dependencia"  id="depename">
+            </div>
+          </div>
+          <div class="row mt-4">
+            <div class="col">
+              <input type="text" class="form-control" placeholder="Director"  id="direc">
+            </div>
+            <div class="col">
+              <input type="text" class="form-control" placeholder="Url"  id="urls">
+            </div>
+          </div>
+        </form>
+        <div class="row mt-4"><div class="col">
+          <textarea rows="4" class="form-control" placeholder="Descrpicion" id="descrip"></textarea>
+        </div>
+      </div>
+      <div class="row mt-4 justify-content-center">
+        <input class="btn btn-info" name="uploadedfile" id="imagens" type="file" />
+      </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+        <button type="button" class="btn btn-outline-success" id="editevento">Aceptar</button>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="modal fade" id="editarnoticia" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="modalTitle2">Editar Noticia</h5>
+        <button type="button" class="close" data-dismiss="modal2" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body" id="bodyModal">
+        <form>
+          <div class="row">
+            <div class="col">
+              <input type="text" class="form-control" placeholder="Id"  id="ids" disabled>
+            </div>
+            <div class="col">
+              <input type="text" class="form-control" placeholder="Dependencia"  id="depename">
+            </div>
+          </div>
+          <div class="row mt-4">
+            <div class="col">
+              <input type="text" class="form-control" placeholder="Director"  id="direc">
+            </div>
+            <div class="col">
+              <input type="text" class="form-control" placeholder="Url"  id="urls">
+            </div>
+          </div>
+        </form>
+        <div class="row mt-4"><div class="col">
+          <textarea rows="4" class="form-control" placeholder="Descrpicion" id="descrip"></textarea>
+        </div>
+      </div>
+      <div class="row mt-4 justify-content-center">
+        <input class="btn btn-info" name="uploadedfile" id="imagens" type="file" />
+      </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+        <button type="button" class="btn btn-outline-success" id="editnoti">Aceptar</button>
+      </div>
+    </div>
+  </div>
+</div>
 
 <script>
 $(document).ready(function(){
@@ -532,6 +618,8 @@ mostrarprog = function(idp,progname,namedepe,respo,despro,imagenpro){
   $('#descriprograma').val(despro);
   $('#imagenprograma').val(imagenpro); 
 };
+</script>
+<script>
 </script>
 <script>
 $(document).ready(function(){
