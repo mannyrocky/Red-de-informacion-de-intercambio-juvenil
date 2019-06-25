e4a57cb6b3acf6f2b2aa0bfbe4614a2b7af16a6f
Fernando Inzunza <ferinzo96@gmail.com>
2019-06-25 16:32:59 -0600

    diseño

--- resources/views/noticias/noticias.blade.php
+++ resources/views/noticias/noticias.blade.php
@@ -1,13 +1,24 @@
 @extends('layouts.layout')
  
 @section('content')
-<div class="container">
-    <img src="../../uploads/{{$noti->imagennoti}}" alt="segunda imagen" 
-    height="300px" width="100%">
-    <h2>{{$noti->titulonoti}}</h2>
-    <h2>{{$noti->autornoti}}</h2><br>
-    <h2>{{$noti->fechanoti}}</h2>
-    <p style="t">{{$noti->Descripcionnot}}
-    </p>
+<div class="container-fluid">
+<div class="row bg-dark justify-content-center d-block" style="color:white;">
+    <p class="text-center lead text-uppercase font-weight-bold" style="font-size:1.8rem">{{$noti->titulonoti}}</p>
+        <p class="text-center lead">{{$noti->fechanoti}} por {{$noti->autornoti}}</p>
+    </div>
+</div>
+<div class="container shadow p-3 mb-5 bg-white rounded">
+    <div class="photo-gallery">
+        <div class="container-fluid">
+            <div class="row photos justify-content-center">
+                <a href="../../uploads/{{$noti->imagennoti}}" data-lightbox="gallery"  style="max-height:400px;">
+                    <img src="../../uploads/{{$noti->imagennoti}}" alt="segunda imagen" height="400px" width="100%" class="shadow p-3 mb-5">
+                </a>
+            </div>
+            <div class="row justify-content-center">
+                <p class="font-weight-light">{{$noti->Descripcionnot}}</p>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
